d11ade03b9324d75897319772398b7e753aa8beb
strahil9810 <strahil1998@abv.bg>
2022-10-11 16:38:49 +0300

    JS for beginner

---
+++ inches-to-centimeter.js
@@ -0,0 +1,6 @@
+function inchesToCentimeter(inches){
+    let inch = parseFloat(inches);
+    let centimeter = inch * 2.54;
+    console.log("Centimeter = " + centimeter);
+}
+inchesToCentimeter(5)
---
+++ square-area.js
@@ -0,0 +1,6 @@
+function squareArea(sideA){
+    let a = parseFloat(sideA);
+    let area = a * a;
+    console.log("Square area = " + area);
+}
+squareArea(5)
